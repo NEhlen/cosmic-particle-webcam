c29c813fbbfa1f38c357296e6da3d294d6cadaab
Niels Ehlen <nehlen@outlook.com>
2024-10-07 19:57:25 +0200

    added options dump

--- cosmic/calibration.py
+++ cosmic/calibration.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import logging
 import sys
 
+import json
+
 logging_file = "main.log"
 logging.basicConfig(filename=logging_file, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,7 +18,7 @@ def calibrate(
     seconds: int = 60,
     run: str = "data/test/testrun",
     cam_id=0,
-    threshold_time=0.0,
+    warmup_time=0.0,
     **kwargs,
 ) -> np.ndarray:
     logger.info("-#" * 30)
@@ -31,9 +33,9 @@ def calibrate(
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)  # auto mode
-    # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)  # manual mode
-    # cap.set(cv2.CAP_PROP_EXPOSURE, -7)
+    # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)  # auto mode
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)  # manual mode
+    cap.set(cv2.CAP_PROP_EXPOSURE, -2)
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -48,7 +50,7 @@ def calibrate(
     while cur_t - start_time <= seconds:
         ret, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if cur_t - start_time > threshold_time:
+        if cur_t - start_time > warmup_time:
             reference += frame
             frames += 1
         cv2.imshow("frame", frame)
@@ -67,6 +69,16 @@ def calibrate(
     plt.imshow(reference, cmap="gray")
     plt.savefig(run + f"/reference_cam{cam_id}.png")
     np.savetxt(run + f"/reference_Cam{cam_id}.npytxt", reference)
+
+    with open(run + f"/config_cam{cam_id}.json", "w+"):
+        json.dumps(
+            {
+                "integration time [s]": seconds,
+                "warmup time [s]": warmup_time,
+                "width [pixels]": width,
+                "height [pixels]": height,
+            }
+        )
     return reference
 
 
@@ -77,27 +89,28 @@ def find_threshold(ref: np.ndarray) -> float:
 if __name__ == "__main__":
     import os
 
-    os.makedirs("data/test/testrun", exist_ok=True)
+    os.makedirs("data/test/logitechc270", exist_ok=True)
 
     ref0 = calibrate(
         180,
-        width=640,
-        height=480,
+        width=1280,
+        height=960,
         cam_id=0,
-        threshold_time=60.0,
+        warmup_time=60.0,
+        run="data/test/logitechc270",
     )  # 1280,  # 720,
-    ref1 = calibrate(
-        180,
-        width=640,
-        height=480,
-        cam_id=2,
-        threshold_time=60.0,
-    )  # 1280,  # 720,
-    ref2 = calibrate(
-        180,
-        width=640,
-        height=480,
-        cam_id=4,
-        threshold_time=60.0,
-    )  # 1280,  # 720,
+    # ref1 = calibrate(
+    #     180,
+    #     width=640,
+    #     height=480,
+    #     cam_id=2,
+    #     warmup_time=60.0,
+    # )  # 1280,  # 720,
+    # ref2 = calibrate(
+    #     180,
+    #     width=640,
+    #     height=480,
+    #     cam_id=4,
+    #     warmup_time=60.0,
+    # )  # 1280,  # 720,
     threshold = find_threshold(ref0)
